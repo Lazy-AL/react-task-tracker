41e89e786c4c57aae7a147411ca149a844321367
Anri <anri.lazashvili@georgiatech.eu>
2021-05-27 00:11:08 +0400

    feat: add task

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,7 +1,7 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks'
 import {useState} from 'react'
-import Task from './components/task';
+import AddTask from './components/AddTask';
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -24,6 +24,14 @@ function App() {
       reminder:false,
   }
   ])
+
+  //add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks,newTask])
+  }
+
   //delete task
   const deleteTask = (id) => {
     setTasks(tasks.filter((Task) => tasks.id !== id))
@@ -40,6 +48,7 @@ function App() {
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask}/>
       {tasks.length > 0 ? <Tasks tasks={tasks}
         onDelete={deleteTask} onToggle={toggleReminder} /> : 'There is no task'}
     </div>
